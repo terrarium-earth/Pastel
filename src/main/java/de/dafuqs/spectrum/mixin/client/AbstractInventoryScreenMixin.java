fbffa2276a3d67452384530166e7c936d966bb1a
Electro_593 <electro5939@gmail.com>
2025-02-01 18:39:45 -0500

    Horrible jank mixin hackery

--- src/main/java/de/dafuqs/spectrum/mixin/client/AbstractInventoryScreenMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/client/AbstractInventoryScreenMixin.java
@@ -8,6 +8,8 @@ import net.minecraft.util.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
+import java.util.*;
+
 @Mixin(AbstractInventoryScreen.class)
 public class AbstractInventoryScreenMixin {
 	
@@ -16,10 +18,16 @@ public class AbstractInventoryScreenMixin {
 		return StatusEffectHelper.getTexture(texture, effect);
 	}
 	
-	// TODO: this mixin fails to get the local
 	@ModifyArg(method = "drawStatusEffectBackgrounds(Lnet/minecraft/client/gui/DrawContext;IILjava/lang/Iterable;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1))
-	private Identifier spectrum$modifyBackground(Identifier texture, @Local(print = true) StatusEffectInstance effect) {
-		return StatusEffectHelper.getTexture(texture, effect);
+	private Identifier spectrum$modifyBackground(Identifier texture, @Local(ordinal = 2) int height, @Local(argsOnly = true) Iterable<StatusEffectInstance> statusEffects, @Local(ordinal = 2) int i) {
+		// This is so cursed... since statusEffectInstance is being compiled out, we get to do this instead
+		HandledScreen<?> handledScreen = (HandledScreen<?>) (Object) this;
+		Iterator<StatusEffectInstance> instance = statusEffects.iterator();
+		for (int y = handledScreen.y; y < i && instance.hasNext(); y += height)
+			instance.next();
+		if (instance.hasNext())
+			return StatusEffectHelper.getTexture(texture, instance.next());
+		return texture;
 	}
-
+	
 }
