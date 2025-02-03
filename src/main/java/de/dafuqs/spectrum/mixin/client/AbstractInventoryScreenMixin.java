92742e86e0a51d88477c68a7773867e0a8d798a3
Electro_593 <electro5939@gmail.com>
2025-02-02 20:35:38 -0500

    More mixins, courtesy of imreallybadatnames

--- src/main/java/de/dafuqs/spectrum/mixin/client/AbstractInventoryScreenMixin.java
+++ src/main/java/de/dafuqs/spectrum/mixin/client/AbstractInventoryScreenMixin.java
@@ -1,6 +1,7 @@
 package de.dafuqs.spectrum.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.*;
+import com.llamalad7.mixinextras.sugar.ref.*;
 import de.dafuqs.spectrum.helpers.*;
 import net.minecraft.client.gui.screen.ingame.*;
 import net.minecraft.entity.effect.*;
@@ -8,10 +9,14 @@ import net.minecraft.util.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
-import java.util.*;
-
 @Mixin(AbstractInventoryScreen.class)
 public class AbstractInventoryScreenMixin {
+	
+	@ModifyVariable(method = "drawStatusEffectBackgrounds(Lnet/minecraft/client/gui/DrawContext;IILjava/lang/Iterable;Z)V", at = @At("STORE"))
+	private StatusEffectInstance spectrum$saveEffect(StatusEffectInstance value, @Share("effect") LocalRef<StatusEffectInstance> effect) {
+		effect.set(value);
+		return value;
+	}
 	
 	@ModifyArg(method = "drawStatusEffectBackgrounds(Lnet/minecraft/client/gui/DrawContext;IILjava/lang/Iterable;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 0))
 	private Identifier spectrum$modifyWideBackground(Identifier texture, @Local StatusEffectInstance effect) {
@@ -19,15 +24,8 @@ public class AbstractInventoryScreenMixin {
 	}
 	
 	@ModifyArg(method = "drawStatusEffectBackgrounds(Lnet/minecraft/client/gui/DrawContext;IILjava/lang/Iterable;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1))
-	private Identifier spectrum$modifyBackground(Identifier texture, @Local(ordinal = 2) int height, @Local(argsOnly = true) Iterable<StatusEffectInstance> statusEffects, @Local(ordinal = 2) int i) {
-		// This is so cursed... since statusEffectInstance is being compiled out, we get to do this instead
-		HandledScreen<?> handledScreen = (HandledScreen<?>) (Object) this;
-		Iterator<StatusEffectInstance> instance = statusEffects.iterator();
-		for (int y = handledScreen.y; y < i && instance.hasNext(); y += height)
-			instance.next();
-		if (instance.hasNext())
-			return StatusEffectHelper.getTexture(texture, instance.next());
-		return texture;
+	private Identifier spectrum$modifyBackground(Identifier texture, @Share("effect") LocalRef<StatusEffectInstance> effect) {
+		return StatusEffectHelper.getTexture(texture, effect.get());
 	}
 	
 }
