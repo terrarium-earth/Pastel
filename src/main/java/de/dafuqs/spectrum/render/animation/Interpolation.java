02956f4212f5ce235c7c19518663359c6d136229
azzyy <azzyypaaras@azzyys.works>
2025-03-24 21:36:13 -0500

    New interpolation types

--- src/main/java/de/dafuqs/spectrum/render/animation/Interpolation.java
+++ src/main/java/de/dafuqs/spectrum/render/animation/Interpolation.java
@@ -5,6 +5,8 @@ import net.minecraft.util.math.*;
 @FunctionalInterface
 public interface Interpolation {
 	Interpolation LINEAR = (start, end, delta) -> MathHelper.lerp(delta, start, end);
+	Interpolation EASE_IN = (start, end, delta) -> MathHelper.lerp(Math.pow(delta, 2), start, end);
+	Interpolation EASE_OUT = (start, end, delta) -> MathHelper.lerp(Math.pow(delta, 0.5), start, end);
 	Interpolation CLAMPED = MathHelper::clampedLerp;
 	// You know, it is kind of fucked up that clamped lerp and lerp don't have the same signature
 	
