541f20feccb9499289f32e04eb0d981d5d11d129
azzyy <azzyypaaras@azzyys.works>
2025-06-01 08:01:53 -0500

    Fix bedrock armor rendering

--- src/main/java/earth/terrarium/pastel/registries/client/SpectrumArmorRenderers.java
+++ src/main/java/earth/terrarium/pastel/registries/client/SpectrumArmorRenderers.java
@@ -2,32 +2,33 @@ package earth.terrarium.pastel.registries.client;
 
 import earth.terrarium.pastel.registries.*;
 import earth.terrarium.pastel.render.armor.*;
+import it.unimi.dsi.fastutil.objects.*;
 import net.minecraft.client.*;
 import net.minecraft.client.model.*;
+import net.minecraft.core.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.item.*;
 import net.neoforged.neoforge.client.extensions.common.*;
+import net.neoforged.neoforge.registries.*;
+
+import java.util.*;
 
 public class SpectrumArmorRenderers {
 
-	public static final Item[] BEDROCK_ARMOR = {
-            SpectrumItems.BEDROCK_HELMET.get(),
-            SpectrumItems.BEDROCK_CHESTPLATE.get(),
-            SpectrumItems.BEDROCK_LEGGINGS.get(),
-            SpectrumItems.BEDROCK_BOOTS.get()
+	public static final DeferredItem<?>[] BEDROCK_ARMOR = {
+            SpectrumItems.BEDROCK_HELMET,
+            SpectrumItems.BEDROCK_CHESTPLATE,
+            SpectrumItems.BEDROCK_LEGGINGS,
+            SpectrumItems.BEDROCK_BOOTS
     };
 
 	public static void register(RegisterClientExtensionsEvent event) {
 		event.registerItem(new IClientItemExtensions() {
-			private HumanoidModel<LivingEntity> model;
+			private Map<EquipmentSlot, HumanoidModel<LivingEntity>> models = new Object2ObjectArrayMap<>();
 
 			@Override
 			public HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
-				if (model == null) {
-					model = provideArmorModelForSlot(equipmentSlot);
-				}
-
-				return model;
+				return models.computeIfAbsent(equipmentSlot, this::provideArmorModelForSlot);
 			}
 
 			private HumanoidModel<LivingEntity> provideArmorModelForSlot(EquipmentSlot slot) {
