f4379e5ac23808305043d4cf04d8399fcdf670e2
azzyy <azzyypaaras@azzyys.works>
2025-06-15 21:40:10 -0500

    Make shooting stars render properly. Fixes #13

--- src/main/java/earth/terrarium/pastel/entity/render/ShootingStarEntityRenderer.java
+++ src/main/java/earth/terrarium/pastel/entity/render/ShootingStarEntityRenderer.java
@@ -2,6 +2,7 @@ package earth.terrarium.pastel.entity.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import earth.terrarium.pastel.entity.entity.ShootingStarEntity;
+import net.minecraft.client.renderer.RenderType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.minecraft.client.Minecraft;
@@ -17,6 +18,7 @@ import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.model.data.ModelData;
 
 @OnlyIn(Dist.CLIENT)
 public class ShootingStarEntityRenderer extends EntityRenderer<ShootingStarEntity> {
@@ -40,7 +42,8 @@ public class ShootingStarEntityRenderer extends EntityRenderer<ShootingStarEntit
 				BlockPos blockpos = BlockPos.containing(shootingStarEntity.getX(), shootingStarEntity.getBoundingBox().maxY, shootingStarEntity.getZ());
 				poseStack.translate(-0.5, 0.0, -0.5);
 				BlockRenderDispatcher blockRenderManager = Minecraft.getInstance().getBlockRenderer();
-				blockRenderManager.getModelRenderer().tesselateBlock(world, blockRenderManager.getBlockModel(blockState), blockState, blockpos, poseStack, vertexConsumerProvider.getBuffer(ItemBlockRenderTypes.getMovingBlockRenderType(blockState)), false, world.random, blockState.getSeed(shootingStarEntity.blockPosition()), OverlayTexture.NO_OVERLAY);
+				blockRenderManager.renderSingleBlock(blockState, poseStack, vertexConsumerProvider, light, OverlayTexture.NO_OVERLAY, ModelData.EMPTY, RenderType.CUTOUT);
+				//blockRenderManager.getModelRenderer().tesselateBlock(world, blockRenderManager.getBlockModel(blockState), blockState, blockpos, poseStack, vertexConsumerProvider.getBuffer(ItemBlockRenderTypes.getMovingBlockRenderType(blockState)), false, world.random, blockState.getSeed(shootingStarEntity.blockPosition()), OverlayTexture.NO_OVERLAY);
 				poseStack.popPose();
 				super.render(shootingStarEntity, yaw, tickDelta, poseStack, vertexConsumerProvider, light);
 			}
