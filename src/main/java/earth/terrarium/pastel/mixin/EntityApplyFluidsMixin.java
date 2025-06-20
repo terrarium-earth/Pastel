0a8ada28092198d4bcd30f6634095830efb7d9f9
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2025-06-20 17:45:30 -0400

    Add missing mixin
    fixes #22
    fixes #28

---
+++ src/main/java/earth/terrarium/pastel/mixin/EntityApplyFluidsMixin.java
@@ -0,0 +1,53 @@
+package earth.terrarium.pastel.mixin;
+
+import earth.terrarium.pastel.api.entity.TouchingWaterAware;
+import earth.terrarium.pastel.registries.SpectrumFluidTags;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.material.Fluid;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
+
+@Mixin(Entity.class)
+public abstract class EntityApplyFluidsMixin implements TouchingWaterAware {
+	
+	@Final
+	@Shadow
+	private Set<TagKey<Fluid>> fluidOnEyes;
+	
+	@Unique
+	private boolean pastel$isActuallyTouchingWater = false;
+	
+	@Override
+	public boolean spectrum$isActuallyTouchingWater() {
+		return this.pastel$isActuallyTouchingWater;
+	}
+	
+	@Override
+	public void spectrum$setActuallyTouchingWater(boolean actuallyTouchingWater) {
+		this.pastel$isActuallyTouchingWater = actuallyTouchingWater;
+	}
+	
+	@Inject(method = "isEyeInFluid", at = @At("RETURN"), cancellable = true)
+	public void spectrum$isSubmergedIn(TagKey<Fluid> fluidTag, CallbackInfoReturnable<Boolean> cir) {
+		if (!cir.getReturnValue() && fluidTag == FluidTags.WATER) {
+			cir.setReturnValue(this.fluidOnEyes.contains(SpectrumFluidTags.SWIMMABLE_FLUID));
+		}
+	}
+	
+	@Inject(method = "isUnderWater", at = @At("RETURN"), cancellable = true)
+	public void spectrum$isSubmergedInWater(CallbackInfoReturnable<Boolean> cir) {
+		if (!cir.getReturnValue() && this.fluidOnEyes.contains(SpectrumFluidTags.SWIMMABLE_FLUID)) {
+			//this.submergedFluidTag.add(FluidTags.WATER);
+			cir.setReturnValue(true);
+		}
+	}
+}
