91ec62bf29f4e3e1ccec8ff7094eb21a53773674
Katrina Renarde <katthefox@proton.me>
2026-01-21 13:07:35 -0800

    fixed ClientLanguageMixin

--- src/main/java/earth/terrarium/pastel/mixin/client/ClientLanguageMixin.java
+++ src/main/java/earth/terrarium/pastel/mixin/client/ClientLanguageMixin.java
@@ -2,6 +2,7 @@ package earth.terrarium.pastel.mixin.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.language.ClientLanguage;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -18,15 +19,15 @@ import java.util.List;
 import java.util.Map;
 
 @Mixin(ClientLanguage.class)
-public class TranslationStorageMixin {
+public class ClientLanguageMixin {
 
     @Mutable
     @Shadow
     @Final
     private Map<String, String> storage;
 
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void addTranslations(Map<String, String> translations, boolean rightToLeft, CallbackInfo ci) {
+    @Inject(method = "Lnet/minecraft/client/resources/language/ClientLanguage;<init>(Ljava/util/Map;ZLjava/util/Map;)V", at = @At("TAIL"))
+    private void addTranslations(Map<String,String> translations, boolean rightToLeft, Map<String, Component> componentStorage, CallbackInfo ci) {
         Calendar calendar = Calendar.getInstance();
         if (calendar.get(Calendar.MONTH) != Calendar.APRIL || calendar.get(Calendar.DAY_OF_MONTH) != 1) return;
 
