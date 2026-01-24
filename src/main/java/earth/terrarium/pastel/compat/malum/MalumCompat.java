71b5be88f82ccdd94575cb96f182c8c056c15e96
Katrina Renarde <katthefox@proton.me>
2026-01-23 20:13:34 -0800

    what the fuck java

--- src/main/java/earth/terrarium/pastel/compat/malum/MalumCompat.java
+++ src/main/java/earth/terrarium/pastel/compat/malum/MalumCompat.java
@@ -2,11 +2,11 @@ package earth.terrarium.pastel.compat.malum;
 
 import com.sammy.malum.registry.common.MalumAttachmentTypes;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.util.Mth;
 
 public class MalumCompat {
     public static int getSoulWardRows(LocalPlayer player) {
-        // java why
-        return Math.toIntExact(Math.round(Math.ceil(player.getData(MalumAttachmentTypes.SOUL_WARD)
-                                                          .getSoulWard() / 30)));
+        return Mth.ceil(player.getData(MalumAttachmentTypes.SOUL_WARD)
+                                                          .getSoulWard() / 30);
     }
 }
