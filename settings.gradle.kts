fc66d37155bdc9aa08121877e4c9a136c0288e7e
Ashley Wright <ashley@msrandom.net>
2026-06-17 20:23:52 -0400

    Add automated linter and formatter

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "pastel"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+
+        maven(url = "https://maven.msrandom.net/repository/cloche/")
+		maven(url = "https://maven.teamresourceful.com/repository/maven-public/")
+    }
+}
